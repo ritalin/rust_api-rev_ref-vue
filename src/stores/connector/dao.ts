6458d3c89fc656141b04b824d16897826b422c61
Ritalin <kazu.ktz.tamura@gmail.com>
2024-05-16 19:44:23 +0900

    全部Parquetにした

--- src/stores/connector/dao.ts
+++ src/stores/connector/dao.ts
@@ -10,24 +10,25 @@ export const createSchemaCore = async (conn: ConnectionWrapper) => {
 }
 
 export const ingestCore = async (conn: ConnectionWrapper, namespace: string) => {
-    conn.runScript(`copy deprecated from '${namespace}/deprecated.parquet' (FORMAT PARQUET)`)
-    conn.runScript(`copy prototype from '${namespace}/prototype.parquet' (FORMAT PARQUET)`)
-    conn.runScript(`copy prototype_crate_ref from '${namespace}/prototype_crate_ref.parquet' (FORMAT PARQUET)`)
-    conn.runScript(`copy prototype_type_ref from '${namespace}/prototype_type_ref.parquet' (FORMAT PARQUET)`)
-    conn.runScript(`copy type_symbol from '${namespace}/type_symbol.parquet' (FORMAT PARQUET)`)
+    // conn.runScript(`copy deprecated from '${namespace}/deprecated.parquet' (FORMAT PARQUET)`)
+    // conn.runScript(`copy prototype from '${namespace}/prototype.parquet' (FORMAT PARQUET)`)
+    // conn.runScript(`copy prototype_crate_ref from '${namespace}/prototype_crate_ref.parquet' (FORMAT PARQUET)`)
+    // conn.runScript(`copy prototype_type_ref from '${namespace}/prototype_type_ref.parquet' (FORMAT PARQUET)`)
+    // conn.runScript(`copy type_symbol from '${namespace}/type_symbol.parquet' (FORMAT PARQUET)`)
     
     const tablenames = [
         "crate_symbol",
-        // "deprecated",
+        "deprecated",
         "prototype_deprecated_ref",
-        // "prototype",
-        // "prototype_crate_ref",
-        // "prototype_type_ref",
-        // "type_symbol",
+        "prototype",
+        "prototype_crate_ref",
+        "prototype_type_ref",
+        "type_symbol",
     ]
 
     for (let name of tablenames) {
-        const sql = `copy ${name} from '${namespace}/${name}.json' (FORMAT 'json', array 'true')`
+        // const sql = `copy ${name} from '${namespace}/${name}.json' (FORMAT 'json', array 'true')`
+        const sql = `copy ${name} from '${namespace}/${name}.parquet' (FORMAT PARQUET)`
         await conn.runScript(sql)
     }
 
