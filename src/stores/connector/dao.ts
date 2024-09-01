eb1b1f3436ecbabf50650d13b41f58737a70b60a
Ritalin <kazu.ktz.tamura@gmail.com>
2024-09-02 01:07:50 +0900

    スキーマのフォルダ構成の変更

--- src/stores/connector/dao.ts
+++ src/stores/connector/dao.ts
@@ -1,12 +1,23 @@
 import type { ConnectionWrapper } from "@/service/core"
 import type { StructRowProxy, Table } from "@apache-arrow/ts"
 
-import dbschema from '@/assets/sql/schema.sql?raw'
+import type_category from '@/assets/schemas/types/type_category.sql?raw'
+import type_kind from '@/assets/schemas/types/type_kind.sql?raw'
+import tableSchemas from '@/assets/schemas/schema.sql?raw'
 
 export type IngestionQuery = (conn: ConnectionWrapper) => Promise<void> 
 
 export const createSchemaCore = async (conn: ConnectionWrapper) => {
-    await conn.runScript(dbschema)
+    await conn.beginTransaction()
+    try {
+        await conn.runQuery(type_category)
+        await conn.runQuery(type_kind)
+        await conn.runScript(tableSchemas)
+        conn.commit()
+    }
+    catch (_) {
+        conn.rollback()
+    }
 }
 
 export const ingestCore = async (conn: ConnectionWrapper, namespace: string) => {
